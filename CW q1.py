cf262cd86386c2de6e83253214c580a737090236
Yegeunator <33755590+Yegeunator@users.noreply.github.com>
2017-11-20 17:46:44 +0000

    Added q1
    Brief command input

---
+++ CW q1.py
@@ -0,0 +1,25 @@
+import math
+
+
+
+def swimming():
+    none=1
+    time =sDistance/speed*none
+
+def cycling():
+    none=1
+    time=cDistance/speed*none
+
+def running():
+    none=1
+    time=rDistance/speed*none
+
+
+sDistance=float(raw_input("Please input a distance for swimming: "))
+cDistance=float(raw_input("Plese input a distnace for cylcing: "))
+rDistance=float(raw_input ("Please input a distance for running: "))
+
+print "For swimming distance you entered:", sDistance,"metres"
+print "For cycling distance you entered:", cDistance,"metres"
+print "For running distance you entered:", rDistance,"metres"
+    
