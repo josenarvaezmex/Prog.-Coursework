f43d874c3db244814f9b642214e7c2ef157fcb2e
JoshuaDonald <33843096+JoshuaDonald@users.noreply.github.com>
2017-11-22 14:49:11 +0000

    Update CW q1.py
    Final edit for Question 1, added table, final sum, fixed spelling errors, converted all times to seconds and butchered Yegeun's code.

--- CW q1.py
+++ CW q1.py
@@ -1,42 +1,51 @@
-import math
-
-
+TotalTime=[]
 
 def swimming():
     none=1
-    speed=6200
+    speed=(6200.0/(60*60))
     time =sDistance/speed*none
-    print "Swimming took %.2f hours"%(time)
+    print "Swimming""    |", "%14.2f"%(time)
+    TotalTime.append(time)
+    
 def cycling():
     none=1
-    speed=52800
+    speed=(52800.0/(60*60))
     time=cDistance/speed*none
-    print "Cycling took %.2f hours"%(time)
-
+    print "Cycling""     |", "%14.2f"%(time)
+    TotalTime.append(time)
+    
 def running():
     none=1
-    speed=18300
+    speed=(18300.0/(60*60))
     time=rDistance/speed*none
-    print "Running took %.2f hours"%(time)
-
-
-sDistance=int(raw_input("Please input a distance for swimming: "))
-cDistance=int(raw_input("Plese input a distnace for cylcing: "))
-rDistance=int(raw_input ("Please input a distance for running: "))
-
+    print "running""     |",  "%14.2f"%(time)
+    TotalTime.append(time)
+    
 while True:
-
+    
+    sDistance=float(raw_input("Please input a distance for swimming: "))
+    cDistance=float(raw_input("Plese input a distnace for cycling: "))
+    rDistance=float(raw_input ("Please input a distance for running: "))
+    
     if sDistance<0 or cDistance<0 or rDistance<0:
-        print "Please input all postive integer numbers"
-        sDistance=int(raw_input("Please input a distance for swimming: "))
-        cDistance=int(raw_input("Plese input a distnace for cylcing: "))
-        rDistance=int(raw_input ("Please input a distance for running: "))
-
+        print "Please input all postive integer numbers,"
+    
+    elif sDistance>=80000 or cDistance>=80000 or rDistance>=80000:
+        print "Please use values below 80000 metres."
+        
     else:
-        print "For swimming distance you entered:", sDistance,"metres"
-        print "For cycling distance you entered:", cDistance,"metres"
-        print "For running distance you entered:", rDistance,"metres"
+        
+        print "Discipline  | Time Taken (s)"
+        print "============|==============="
         swimming()
         cycling()
         running()
+        
+        print "For swimming distance you entered:", sDistance,"metres"
+        print "For cycling distance you entered:", cDistance,"metres"
+        print "For running distance you entered:", rDistance,"metres"
+
+        print "The total time for Johnny to complete a race is:",\
+        "%.2f"%(sum(TotalTime)), "Seconds."
+        
         break
