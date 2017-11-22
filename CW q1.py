85d6402c700d7342316464ffe09b6802f1a21dee
Yegeunator <33755590+Yegeunator@users.noreply.github.com>
2017-11-22 12:30:22 +0000

    Update CW q1.py

--- CW q1.py
+++ CW q1.py
@@ -4,22 +4,39 @@ import math
 
 def swimming():
     none=1
+    speed=6200
     time =sDistance/speed*none
-
+    print "Swimming took %.2f hours"%(time)
 def cycling():
     none=1
+    speed=52800
     time=cDistance/speed*none
+    print "Cycling took %.2f hours"%(time)
 
 def running():
     none=1
+    speed=18300
     time=rDistance/speed*none
+    print "Running took %.2f hours"%(time)
 
 
-sDistance=float(raw_input("Please input a distance for swimming: "))
-cDistance=float(raw_input("Plese input a distnace for cylcing: "))
-rDistance=float(raw_input ("Please input a distance for running: "))
+sDistance=int(raw_input("Please input a distance for swimming: "))
+cDistance=int(raw_input("Plese input a distnace for cylcing: "))
+rDistance=int(raw_input ("Please input a distance for running: "))
 
-print "For swimming distance you entered:", sDistance,"metres"
-print "For cycling distance you entered:", cDistance,"metres"
-print "For running distance you entered:", rDistance,"metres"
-    
+while True:
+
+    if sDistance<0 or cDistance<0 or rDistance<0:
+        print "Please input all postive integer numbers"
+        sDistance=int(raw_input("Please input a distance for swimming: "))
+        cDistance=int(raw_input("Plese input a distnace for cylcing: "))
+        rDistance=int(raw_input ("Please input a distance for running: "))
+
+    else:
+        print "For swimming distance you entered:", sDistance,"metres"
+        print "For cycling distance you entered:", cDistance,"metres"
+        print "For running distance you entered:", rDistance,"metres"
+        swimming()
+        cycling()
+        running()
+        break
