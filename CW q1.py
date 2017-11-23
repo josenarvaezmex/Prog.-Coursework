f8c5d4d33330bbe00b5018013c2338cc469bb561
Yegeunator <33755590+Yegeunator@users.noreply.github.com>
2017-11-23 00:52:08 +0000

    Update CW q1.py

--- CW q1.py
+++ CW q1.py
@@ -1,31 +1,39 @@
+##This programm is calculates how fast Johnny can do a triathalaon
 TotalTime=[]
 
 def swimming():
-    none=1
-    speed=(6200.0/(60*60))
-    time =sDistance/speed*none
+    equipment = 1
+    speed=6200.0/3600
+    time =sDistance/speed*equipment
     print "Swimming""    |", "%14.2f"%(time)
     TotalTime.append(time)
+    ## This function calculates the speed and time it takes for
+    ## for swimming
     
 def cycling():
-    none=1
-    speed=(52800.0/(60*60))
+    equipment = 1
+    speed=52800.0/3600
     time=cDistance/speed*none
     print "Cycling""     |", "%14.2f"%(time)
     TotalTime.append(time)
+    ## This function calculates the speed and time it takes for
+    ## for cycling
     
 def running():
-    none=1
-    speed=(18300.0/(60*60))
-    time=rDistance/speed*none
+    equipment = 1
+    speed=18300.0/3600
+    time=rDistance/speed*equipment
     print "running""     |",  "%14.2f"%(time)
     TotalTime.append(time)
+    ## This function calculates the speed and time it takes for
+    ## for running
     
 while True:
     
-    sDistance=float(raw_input("Please input a distance for swimming: "))
-    cDistance=float(raw_input("Plese input a distnace for cycling: "))
-    rDistance=float(raw_input ("Please input a distance for running: "))
+    sDistance = float(raw_input("Please input a distance for swimming: "))
+    cDistance = float(raw_input("Plese input a distance for cycling: "))
+    rDistance = float(raw_input ("Please input a distance for running: "))
+    ## creates a input of all the distances 
     
     if sDistance<0 or cDistance<0 or rDistance<0:
         print "Please input all postive integer numbers,"
@@ -40,6 +48,8 @@ while True:
         swimming()
         cycling()
         running()
+
+        ## This creates a table for Discipline and time taken
         
         print "For swimming distance you entered:", sDistance,"metres"
         print "For cycling distance you entered:", cDistance,"metres"
@@ -47,5 +57,6 @@ while True:
 
         print "The total time for Johnny to complete a race is:",\
         "%.2f"%(sum(TotalTime)), "Seconds."
+        #this prints out all the distances travled and the speed of travel.
         
         break
